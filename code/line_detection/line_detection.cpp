5e333789d0363e6300c2ab6a0a5d7386d5eb290a
root <root@SHITBOX>
2024-03-09 18:12:42 +1100

    reducing bounds around image

--- code/line_detection/line_detection.cpp
+++ code/line_detection/line_detection.cpp
@@ -1,3 +1,11 @@
+/*
+
+Ideas/improvements:
+- add contrast adjustment and see how it affects the detail
+
+*/
+
+
 // line_detection.cpp
 #include "line_detection.h"
 #include <iostream>
@@ -16,7 +24,7 @@ void Line_detection::begin()
     // find number of files inside data->faces folder
     int num_faces = std::distance(std::filesystem::directory_iterator("../data/faces"), std::filesystem::directory_iterator{});
 
-    std::cout << "choose a value from 0 - " << (num_faces-1) << " to select a face: ";
+    std::cout << "choose a value from 0 - " << (num_faces - 1) << " to select a face: ";
     std::cin >> value;
 
     switch (value)
@@ -36,6 +44,9 @@ void Line_detection::begin()
     case 4:
         input_image = cv::imread("../data/faces/zuck_HD.webp", cv::IMREAD_COLOR);
         break;
+    case 5:
+        input_image = cv::imread("../data/faces/group.webp", cv::IMREAD_COLOR);
+        break;
     default:
         std::cout << "Invalid input" << std::endl;
         break;
@@ -51,21 +62,153 @@ void Line_detection::begin()
     // cv::resize(input_image, input_image, cv::Size(), 0.5, 0.5, cv::INTER_LINEAR);
     cv::Mat edgeImage = edgeDetection(input_image);
 
+    std::vector<std::vector<float>> lines = convertToNormalisedLines(edgeImage);
+
+    // create something to store shaded regions. Use k-means clustering
+
     cv::imshow("Image", edgeImage);
     cv::waitKey(0); // Wait indefinitely for a key press
+}
+
+std::vector<std::vector<float>> Line_detection::convertToNormalisedLines(cv::Mat &edgeImageBW)
+{
+    std::vector<std::vector<float>> lines;
+
+    // first check if it is black and white
+    if (!isBinary(edgeImageBW))
+    {
+        std::cout << "Input is not black and white" << std::endl;
+        return lines;
+    }
+
+    // ensure there are more white pixels than black. This should be the case...
+    // ...if the image has been edge extracted, contains black lines.
+    uint whitePixels = colourCount(edgeImageBW, 255);
+    uint blackPixels = colourCount(edgeImageBW, 0);
+
+    if (blackPixels > whitePixels)
+    {
+        std::cout << "more black pixels than white" << std::endl;
+        return lines;
+    }
+
+    // next crop the image to the max and mins of the black pixels
+    // then resize it to some universal value
+    cv::Rect boundaryBox = findImageBounds(edgeImageBW);
+
+    cv::Mat croppedImage = edgeImageBW(boundaryBox);
+
+    cv::imshow("Image", croppedImage);
+    cv::waitKey(0); // Wait indefinitely for a key press
+
+    // use the concept of momentum. If the line is being drawn in a similar direction,...
+    // ... keep it apart of the same row.
+    // need to keep track of what pixels have been visited
+
+    // each row represents a new line. keep track of the average angle of this row,
+    // if the next line creates an angle greater than some threshold, start new line
+
+    // implement Douglas-Peucker algorithm to point count on lines
+
+    return lines;
+}
+
+cv::Rect Line_detection::findImageBounds(cv::Mat &image)
+{
+    uint maxX = 0;
+    uint minX = 99999999; // big boy
+
+    uint maxY = 0;
+    uint minY = 99999999; // big boy
+
+    for (uint i = 0; i < image.rows; ++i)
+    {
+        for (uint j = 0; j < image.cols; ++j)
+        {
+            if (image.at<uchar>(i, j) == 0)
+            {
+                if (i > maxY)
+                {
+                    maxY = i;
+                }
+                if (i < minY)
+                {
+                    minY = i;
+                }
+
+                if (j > maxX)
+                {
+                    maxX = j;
+                }
+                if (j < minX)
+                {
+                    minX = j;
+                }
+            }
+        }
+    }
+
+    cv::Rect rect(minX, minY, (maxX - minX), (maxY - minY));
+    return rect;
+}
+
+uint Line_detection::colourCount(const cv::Mat &image, int colourValue)
+{
+    uint count = 0;
+    for (uint i = 0; i < image.rows; ++i)
+    {
+        for (uint j = 0; j < image.cols; ++j)
+        {
+            uchar pixelValue = image.at<uchar>(i, j);
+            if (pixelValue == colourValue)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+bool Line_detection::isBinary(const cv::Mat &image)
+{
+    // Ensure it's grayscale first
+    if (!isGrayscale(image))
+    {
+        std::cout << "image is not grayscale" << std::endl;
+        return false;
+    }
+
+    for (int i = 0; i < image.rows; ++i)
+    {
+        for (int j = 0; j < image.cols; ++j)
+        {
+            uchar pixelValue = image.at<uchar>(i, j);
+            if (pixelValue != 0 && pixelValue != 255)
+            {
+                return false; // Found a pixel that is neither black nor white
+            }
+        }
+    }
+    return true; // All pixels are either black or white
+}
+
+bool Line_detection::isGrayscale(const cv::Mat &image)
+{
+    // Check if image is single channel
+    return image.channels() == 1;
 }
 
 // Method to set the value
 cv::Mat Line_detection::edgeDetection(cv::Mat &input_image)
 {
-    // rescale image and maintain aspect ratio
+    // maintain image aspect ratio
     int rows = 800;
     int cols = static_cast<int>((static_cast<double>(input_image.cols) / input_image.rows) * rows);
 
     // Resize the image to the new dimensions
     cv::resize(input_image, input_image, cv::Size(cols, rows), 0, 0, cv::INTER_LINEAR);
 
-    // remove background
+    // finds face and removes background
     input_image = backgroundSubtraction(input_image);
 
     // blur image
@@ -81,6 +224,18 @@ cv::Mat Line_detection::edgeDetection(cv::Mat &input_image)
 
     cv::Canny(gray, edge, 50, 150, 3);
     edge.convertTo(draw, CV_8U);
+
+    // maintain image aspect ratio
+    rows = 500;
+    cols = static_cast<int>((static_cast<double>(input_image.cols) / input_image.rows) * rows);
+
+    // Resize the image to the new dimensions
+    cv::resize(draw, draw, cv::Size(cols, rows), 0, 0, cv::INTER_LINEAR);
+
+    cv::bitwise_not(draw, draw); // inverting the binary image and storing it in inverted_binary_image matrix//
+
+    // ensure it is a black and white iamge
+    cv::threshold(draw, draw, 200, 255, cv::THRESH_BINARY);
 
     return draw;
 }
@@ -130,12 +285,34 @@ cv::Rect Line_detection::FaceLocationDetection(cv::Mat &input_image)
     // Detect faces
     faceCascade.detectMultiScale(input_image, faces);
 
-    if (faces.size() > 1)
+    int numFaces = faces.size();
+
+    cv::Rect face;
+
+    if (numFaces > 1)
     {
-        std::cout << "Multiple faces detected. Returning the first detected face." << std::endl;
+        std::cout << numFaces << " faces detected. Returning the first detected face." << std::endl;
+
+        float maxArea = 0;
+        int maxAreaIndex = 0;
+
+        // find the face with the largest bounding box
+        for (int i = 0; i < numFaces; i++)
+        {
+            if (faces[i].area() > maxArea)
+            {
+                maxArea = faces[i].area();
+                maxAreaIndex = i;
+            }
+        }
+        face = faces[maxAreaIndex];
+    }
+    else
+    {
+        face = faces[0];
     }
 
-    cv::Rect expandedFaceRect = expandRectanglePercentage(input_image, faces[0], 20);
+    cv::Rect expandedFaceRect = expandRectanglePercentage(input_image, face, 20);
 
     return expandedFaceRect;
 }
--- code/line_detection/line_detection.h
+++ code/line_detection/line_detection.h
@@ -14,6 +14,16 @@ private:
     cv::Rect FaceLocationDetection(cv::Mat &input_image);
     cv::Rect expandRectanglePercentage(cv::Mat &input_image, const cv::Rect &originalRect, int percentage);
 
+    // expects a black and white (lines are black) image that has been reduces to edges
+    std::vector<std::vector<float>> convertToNormalisedLines(cv::Mat &edgeImageBW);
+
+    bool isBinary(const cv::Mat &image);
+    bool isGrayscale(const cv::Mat &image);
+    uint colourCount(const cv::Mat &image, int colourValue);
+
+    cv::Rect findImageBounds(cv::Mat &edgeImageBW);
+
+
 public:
     Line_detection(int initialValue); // Constructor
     cv::Mat edgeDetection(cv::Mat &input_image);
